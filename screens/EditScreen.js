09dcf39ae6da5e96d61525fc52ec2308be0f3ff6
gamer4life1 <uyenwontanswer@gmail.com>
2020-04-02 19:05:25 -0700

    End of session 4/2/2020

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,13 +1,22 @@
 import * as React from "react";
 import { StyleSheet, Text, View, TextInput, Picker } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import allowance from '../lib/allowance'
 import useAllowance from '../lib/allowanceHook'
 
 export default function EditScreen() {
 
-  const { balancesCents, isLoaded, isLoading, error, weeksSinceStart } = useAllowance();
+  const { balances, isLoaded, isLoading, error, weeksSinceStart } = useAllowance();
   const [currentAccount, setCurrentAccount] = React.useState('savings');
-  const [textboxValue, onChangeTextboxValue] = React.useState(balancesCents[currentAccount]);
+  const [textboxValue, onChangeTextboxValue] = React.useState('');
+  
+  React.useEffect(() => {
+    onChangeTextboxValue(balances[currentAccount])
+  }, [balances, currentAccount])
+
+  const handleTextboxChange = (text) => {
+    onChangeTextboxValue(text)
+  }
 
   return (
     <View
@@ -23,7 +32,7 @@ export default function EditScreen() {
       <Text style={styles.text}>&nbsp;</Text>
       <View style={{width: '600px' }}>
 
-        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} value={textboxValue} onChangeText={text => onChangeTextboxValue(text)} />
+        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} value={textboxValue} onChangeText={text => handleTextboxChange(text)} />
         <Text style={styles.text}>&nbsp;</Text>
         <Text style={styles.text}>&nbsp;</Text>
         <Picker
@@ -32,9 +41,21 @@ export default function EditScreen() {
           onValueChange={(itemValue, itemIndex) => setCurrentAccount(itemValue)}
         >
         <Picker.Item label="Savings" value="savings" />
-        <Picker.Item label="Charity" value="charity" />
         <Picker.Item label="Instant Spending" value="instantSpending" />
+        <Picker.Item label="Charity" value="charity" />
       </Picker>
+        <Text style={styles.text}>&nbsp;</Text>
+        <Text style={styles.text}>&nbsp;</Text>
+
+        <TouchableOpacity
+          onPress={() => {
+            const val = Math.round(100 * parseFloat(textboxValue.replace(/[$,]/g, '')))
+            allowance.setBalance(currentAccount, val)
+          }}
+          style={{ backgroundColor: 'blue', width: '200px' }}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>Save</Text>
+        </TouchableOpacity>
+
         <Text style={styles.text}>&nbsp;</Text>
         <Text style={styles.text}>&nbsp;</Text>
 
