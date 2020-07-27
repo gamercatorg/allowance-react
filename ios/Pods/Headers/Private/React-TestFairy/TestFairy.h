d04325807597c5e22e055b418bf995dc3a3ddfb2
GamerCat Meow <uyenwontanswer@gmail.com>
2020-07-26 17:48:01 -0700

    ejected

---
+++ ios/Pods/Headers/Private/React-TestFairy/TestFairy.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-testfairy/ios/TestFairy.h
---
+++ ios/Pods/Target Support Files/React-TestFairy/React-TestFairy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_React_TestFairy : NSObject
+@end
+@implementation PodsDummy_React_TestFairy
+@end
